881b25ad239326f2c4b4aed329522725916431c7
miguelCidPaz <miguel.cid.paz@gmail.com>
2025-02-12 22:55:26 +0100

    primer commit

---
+++ src/main/java/com/mcproductions/bp/detectionip/DetectionIpApplication.java
@@ -0,0 +1,13 @@
+package com.mcproductions.bp.detectionip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionIpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionIpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcproductions/bp/detectionip/DetectionIpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mcproductions.bp.detectionip;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionIpApplicationTests {
+
+	@Test
+	@Disabled // <---- Agregar esto para deshabilitarlo temporalmente
+	void contextLoads() {
+	}
+
+}
